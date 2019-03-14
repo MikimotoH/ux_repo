cab31be1ace2df16e9b3f53e49a809e40bf37d44
MikimotoH <MikimotoH@gmail.com>
2019-03-14 07:36:19 +0000

    rename bucketName

--- boto3x.py
+++ boto3x.py
@@ -10,9 +10,9 @@ import boto3
 
 
 def upload_file(f_url, local_f, contentType, lastModified):
-    production_phase = False
+    production_phase = True
     if production_phase == True:
-        bucketName = 'grid-linux-bucket'
+        bucketName = 'grid-linux-harvest'
         aws_service_type = 'sqs'
         sqs_url='https://sqs.us-west-2.amazonaws.com/745063655428/grid_linux_harvest'
     else:
