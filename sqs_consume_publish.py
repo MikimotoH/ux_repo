2af3ec7a565328cc1211115f8ae5a32299aa3030
MikimotoH <MikimotoH@gmail.com>
2019-03-23 11:08:56 +0000

    surrect from prev step by .sqs_step; add progressbar

--- sqs_consume_publish.py
+++ sqs_consume_publish.py
@@ -11,6 +11,7 @@ from pprint import pformat
 import boto3
 from botocore.exceptions import ClientError
 import humanfriendly
+from tqdm import tqdm
 from unpack_archive import NotSupportedFileType, unpack_archive
 
 logger = logging.getLogger(__name__)
@@ -41,80 +42,159 @@ def main():
                         stream=sys.stdout, level=logging.INFO)
 
     os.makedirs(dl_dir, exist_ok=True)
-    client = boto3.client('sqs')
+    sqs = boto3.client('sqs')
     s3 = boto3.resource('s3')
+
+    try:
+        with open('.sqs_step', 'r') as fin:
+            step = fin.readline().strip()
+            if step == 'recvMsg':
+                pass
+            elif step == 'delMsg':
+                remains = fin.read()
+                response = json.loads(remains)
+            elif step == 'downloadFile':
+                remains = fin.readline()
+                bucket, key = [_.strip() for _ in remains.split(' ', maxsplit=1)]
+            elif step == 'unpack_archive':
+                local_file = fin.readline().strip()
+                bucket = fin.readline().strip()
+            elif step == 'getDirSize':
+                ext_total_bytes = int(fin.readline().strip())
+                bucket = fin.readline().strip()
+            elif step == 'uploadFiles':
+                ext_total_bytes = int(fin.readline().strip())
+                bucket = fin.readline().strip()
+                children = list()
+                for l in fin:
+                    l = l.strip()
+                    if not l:
+                        continue
+                    children.append(l)
+    except FileNotFoundError:
+        step = ''
+
+    def hook(t: tqdm):
+        def inner(bytes_amount):
+            t.update(bytes_amount)
+        return inner
 
     while True:
         try:
-            logger.info("sqs.receive_message " + SqsUrl)
-            response = client.receive_message(QueueUrl=SqsUrl)
-            response_code = response['ResponseMetadata']['HTTPStatusCode']
-            logger.debug('response_code = %s' % response_code)
-            try:
-                message_body = response['Messages'][0]['Body']
-            except KeyError:
-                logger.info("KeyError MessageBody not exist  %s" % pformat(response))
-                continue
-            receipt_handle = response['Messages'][0]['ReceiptHandle']
-            logger.debug("Deleting Message: %s" % receipt_handle)
-            try:
-                client.delete_message(
-                    QueueUrl=SqsUrl,
-                    ReceiptHandle=receipt_handle,
-                )
-            except ClientError as e:
-                logger.warning("delete_message error: %s" % pformat(e))
+            if step in ['', 'end', 'recvMsg']:
+                step = 'recvMsg'
+                logger.info("sqs.receive_message " + SqsUrl)
+                response = sqs.receive_message(QueueUrl=SqsUrl)
+                response_code = response['ResponseMetadata']['HTTPStatusCode']
+                logger.debug('response_code = %s' % response_code)
+                try:
+                    message_body = response['Messages'][0]['Body']
+                except (KeyError, IndexError):
+                    logger.info("MessageBody not exist  %s" % pformat(response))
+                    continue
 
-            bucket = json.loads(message_body)['bucket']
-            key = json.loads(message_body)['key']
-            logger.debug('key=' + key)
+            if step in ['recvMsg', 'delMsg']:
+                if step == 'delMsg':
+                    assert response
+                step = 'delMsg'
+                receipt_handle = response['Messages'][0]['ReceiptHandle']
+                logger.debug("Delete Message: %s" % receipt_handle)
+                try:
+                    sqs.delete_message(QueueUrl=SqsUrl, ReceiptHandle=receipt_handle)
+                except ClientError as e:
+                    logger.warning("delete_message error: %s" % pformat(e))
 
-            local_file = pjoin(dl_dir, key.split('/')[-1])
-            try:
-                keysize = s3.Bucket(bucket).Object(key).content_length
-                logger.info('download size=%s s3://%s/%s' % (humanfriendly.format_size(keysize), bucket, key))
-                s3.Bucket(bucket).download_file(key, local_file)
-            except ClientError as e:
-                if e.response['Error']['Code'] == "404":
-                    logger.warning('Bucket Key Not exist: ' + key)
-                else:
-                    logger.warning('Key "%s" exception %s' % (key, pformat(e)))
-            except Exception as e:
-                logger.warning(pformat(e))
-                traceback.print_exc()
-                continue
+            if step in ['delMsg', 'downloadFile']:
+                if step == 'downloadFile':
+                    assert bucket
+                    assert key
+                elif step == 'delMsg':
+                    bucket = json.loads(message_body)['bucket']
+                    key = json.loads(message_body)['key']
+                step = 'downloadFile'
+                logger.debug('key=' + key)
+                local_file = pjoin(dl_dir, key.split('/')[-1])
+                try:
+                    s3_obj = s3.Bucket(bucket).Object(key)
+                    fileSize = s3_obj.content_length
+                    logger.info('download size=%s s3://%s/%s' % (humanfriendly.format_size(fileSize), bucket, key))
+                    with tqdm(total=fileSize, ncols=100) as t:
+                        s3_obj.download_file(local_file, Callback=hook(t))
+                except ClientError as e:
+                    if e.response['Error']['Code'] == "404":
+                        logger.warning('Bucket Key Not exist: ' + key)
+                    else:
+                        logger.warning('Key "%s" exception %s' % (key, pformat(e.response)))
 
-            try:
-                os.makedirs(ext_dir, exist_ok=True)
-                children = [pjoin(ext_dir, f) for f, _ in unpack_archive(abspath(local_file), abspath(ext_dir))]
-                logger.info('prepared to upload %s files', len(children))
-                proc = subprocess.Popen("du '%s' -sh" % ext_dir, shell=True, stdout=subprocess.PIPE,
-                                        universal_newlines=True, bufsize=1)
-                du_str, _ = proc.communicate(timeout=1.0)
-                logger.info('extracted file size is %s', du_str.split()[0])
-                for f in children:
-                    child_key = '_extracted/' + guen_keyname(getSha256(f))
-                    try:
-                        logger.debug('upload child "%s" to s3://%s/%s"' % (f, bucket, child_key))
-                        s3.Bucket(bucket).upload_file(f, child_key)
-                    except ClientError as e:
-                        logger.warning('Failed to upload(%s, %s) %s' % (f, child_key, pformat(e)))
-            except NotSupportedFileType:
-                logger.warning('NotSupportedFileType: %s' % local_file)
-                shutil.rmtree(ext_dir, ignore_errors=True)
-            except Exception as e:
-                logger.warning(pformat(e))
-                traceback.print_exc()
-                shutil.rmtree(ext_dir, ignore_errors=True)
-            else:
-                shutil.rmtree(ext_dir, ignore_errors=True)
+            if step in ['downloadFile', 'unpack_archive']:
+                if step == 'unpack_archive':
+                    assert local_file
+                step = 'unpack_archive'
+                try:
+                    os.makedirs(ext_dir, exist_ok=True)
+                    children = [pjoin(ext_dir, f) for f, _ in unpack_archive(abspath(local_file), abspath(ext_dir))]
+                except NotSupportedFileType:
+                    logger.warning('NotSupportedFileType: %s' % local_file)
+                    shutil.rmtree(ext_dir, ignore_errors=True)
+                    continue
                 os.remove(local_file)
-        except KeyboardInterrupt:
-            logger.info('broken by user')
+
+            if step in ['getDirSize', 'unpack_archive']:
+                step = 'getDirSize'
+                proc = subprocess.Popen("du '%s' -sb" % ext_dir, shell=True, stdout=subprocess.PIPE,
+                                        universal_newlines=True, bufsize=1)
+                du_str, _ = proc.communicate()
+                ext_total_bytes = int(du_str.split()[0])
+                logger.info('extracted file size is %s', humanfriendly.format_size(ext_total_bytes))
+
+            if step in ['uploadFiles', 'getDirSize']:
+                if step == 'uploadFiles':
+                    assert children
+                step = 'uploadFiles'
+                try:
+                    logger.info('prepared to upload %s files' % len(children))
+                    with tqdm(total=ext_total_bytes, unit='B', unit_scale=True, desc='upload', ncols=100, leave=False) as t:
+                        for f in children:
+                            child_key = '_extracted/' + guen_keyname(getSha256(f))
+                            try:
+                                logger.debug('upload child "%s" to s3://%s/%s"' % (f, bucket, child_key))
+                                fileSize = os.path.getsize(f)
+                                s3.Bucket(bucket).upload_file(f, child_key)
+                            except ClientError as e:
+                                logger.warning('Failed to upload(%s, %s) %s' % (f, child_key, pformat(e)))
+                            t.update(fileSize)
+                except Exception as e:
+                    logger.warning(pformat(e))
+                    traceback.print_exc()
+                shutil.rmtree(ext_dir, ignore_errors=True)
+
+            step = 'end'
+        except (KeyboardInterrupt, Exception) as e:
+            if isinstance(e, KeyboardInterrupt):
+                logger.info('broken by user')
+            else:
+                logger.warning(pformat(e))
+                logger.warning(traceback.format_exc())
+            with open('.sqs_step', 'w') as fout:
+                fout.write(step + '\n')
+                if step == 'recvMsg':
+                    pass
+                elif step == 'delMsg':
+                    fout.write(json.dumps(response, indent=2))
+                elif step == 'downloadFile':
+                    fout.write("%s %s\n" % (bucket, key))
+                elif step == 'unpack_archive':
+                    fout.write(local_file + '\n')
+                    fout.write(bucket + '\n')
+                elif step == 'getDirSize':
+                    fout.write('%s\n' % ext_total_bytes)
+                    fout.write(bucket + '\n')
+                elif step == 'uploadFiles':
+                    fout.write('%s\n' % ext_total_bytes)
+                    fout.write(bucket + '\n')
+                    for f in children:
+                        fout.write(f + '\n')
             return
-        except Exception as e:
-            logger.warning(pformat(e))
-            logger.warning(traceback.format_exc())
 
 
 if __name__ == '__main__':
